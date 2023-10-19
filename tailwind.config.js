cb7b45102c45197e4e1e41caf1ae54c3eb7a5b82
Boey01 <boeyshuwei@gmail.com>
2023-10-19 23:56:29 +0800

    neon effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 const plugin = require("tailwindcss/plugin");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
     extend: {
       textShadow: {
-        DEFAULT: " 0px 0px 20px var(--tw-shadow-color)",
+        DEFAULT:
+          " 0px 0px 7px #ffff, 0px 0px 14px #ffff, 0px 0px 21px var(--tw-shadow-color), 0px 0px 28px var(--tw-shadow-color), 0px 0px 35px var(--tw-shadow-color) ",
       },
       fontSize: {
         "4.5xl": "2.5rem",
