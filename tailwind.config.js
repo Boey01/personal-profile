1b11c8f88a8ac31cd40c7945b66d40048776e916
Boey01 <boeyshuwei@gmail.com>
2023-10-19 21:54:25 +0800

    basic title formations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      textShadow: {
+        DEFAULT: " 0px 0px 20px var(--tw-shadow-color)",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") },
+      );
+    }),
+  ],
   darkMode: "class",
 };
