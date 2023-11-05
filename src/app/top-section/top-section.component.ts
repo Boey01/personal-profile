7ed1e2ce62769e5bffdda9d0a84be89f64173354
Boey01 <boeyshuwei@gmail.com>
2023-11-05 21:29:42 +0800

    basic card flip effect

--- src/app/top-section/top-section.component.ts
+++ src/app/top-section/top-section.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-top-section',
   templateUrl: './top-section.component.html',
-  styleUrls: ['./top-section.component.css']
+  styleUrls: ['./top-section.component.css'],
 })
-export class TopSectionComponent {
+export class TopSectionComponent implements OnInit {
+  ngOnInit() {
+    const card = document.querySelector('.flip-card');
+    const cardInner = document.querySelector('.flip-card-inner');
 
+    // Function to toggle the card flip
+    function toggleCardFlip() {
+      if (card) {
+        card.classList.toggle('card-flipped');
+      }
+    }
+
+    // Flip the card on load after 0.5 seconds
+    setTimeout(() => {
+      toggleCardFlip();
+    }, 500);
+
+    // Flip the card on click
+    if (card) {
+      card.addEventListener('click', toggleCardFlip);
+    }
+  }
 }
